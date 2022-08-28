f7d58caf44b24d705a570999cc080261d5e9db6f
Wolve1001 <82719580+Wolve1001@users.noreply.github.com>
2022-08-28 19:40:42 +0530

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,11 @@
+i=input('Greeting:').strip().lower()
+if i[0]=="h":
+    if i[1]=="e":
+        if i[2]=="l":
+            print("$0")
+        else:
+            print("$20")
+    else:
+        print("$20")
+else:
+    print("$100")
